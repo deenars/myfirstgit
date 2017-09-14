2151fcd1548895443100f68ff65ac39989bbebc3
mac User <mac@Seemattis-Mac-mini.local>
2017-09-14 12:33:30 +0530

    Please track my file

--- Mynewfirstgit/ViewController.swift
+++ Mynewfirstgit/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("this is my firs Git Project in Git Hub.")
     }
 
     override func didReceiveMemoryWarning() {
